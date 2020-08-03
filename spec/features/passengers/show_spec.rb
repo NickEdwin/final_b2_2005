6452c016f260119dbc271ec43d007dd164bba9e2
Nick Edwin <nickedwin85@gmail.com>
2020-08-03 11:12:16 -0600

    Test for adding a flight to a passenger

--- spec/features/passengers/show_spec.rb
+++ spec/features/passengers/show_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'As a user', type: :feature do
       @flight1 = Flight.create(number: 1234, date: "01/01/01", time: "01:01:01", departure_city: "New York", arrival_city: "Denver", airline_id: @airline.id)
       @flight2 = Flight.create(number: 4321, date: "02/02/02", time: "02:02:02", departure_city: "London", arrival_city: "Miami", airline_id: @airline.id)
       @flight3 = Flight.create(number: 1111, date: "03/03/03", time: "03:03:03", departure_city: "Tokyo", arrival_city: "Chicago", airline_id: @airline.id)
+      Flight.create(number: 2222, date: "04/04/04", time: "04:04:04", departure_city: "Austin", arrival_city: "Quebec", airline_id: @airline.id)
       @passenger1 = Passenger.create(name: "Nick", age: 35)
       FlightPassenger.create(flight_id: @flight1.id, passenger_id: @passenger1.id)
       FlightPassenger.create(flight_id: @flight2.id, passenger_id: @passenger1.id)
@@ -28,6 +29,22 @@ RSpec.describe 'As a user', type: :feature do
 
       click_on("Flight: #{@flight1.number}")
       expect(current_path).to eq("/flights/#{@flight1.id}")
+    end
+
+    it "displays a form to add this passenger to a flight" do
+      visit("/passengers/#{@passenger1.id}")
+
+      expect(page).to_not have_content("Flight: 2222")
+
+      expect(page).to have_content("Add a flight:")
+
+      fill_in :number, with: 2222
+
+      click_on("Add Flight")
+
+      expect(current_path).to eq("/passengers/#{@passenger1.id}")
+
+      expect(page).to have_content("Flight: 2222")
     end
   end
 end
