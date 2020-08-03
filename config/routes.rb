68b8aa43964376d823d96ffb1177bb203c52529a
Nick Edwin <nickedwin85@gmail.com>
2020-08-03 11:12:32 -0600

    New routes for adding passenger to a flight

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "/flights/:id", to: "flights#show"
 
   get "/passengers/:id", to: "passengers#show"
-  
+  post "/passengers/:id/flights/new", to: "flight_passengers#create"
+
 end
