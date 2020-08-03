01b47af472575e04641f3d323684f3ba61798166
Nick Edwin <nickedwin85@gmail.com>
2020-08-03 09:45:04 -0600

    Updated relationships

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -2,4 +2,6 @@ class Airline < ApplicationRecord
   validates_presence_of :name
 
   has_many :flights
+  # has_many :flight_passengers, through: :flights
+  # has_many :flight_passengers, through: :passengers
 end
