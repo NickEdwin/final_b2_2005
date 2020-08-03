3af1598aa0a691ff68af2f23c5480e2dbebf3039
Nick Edwin <nickedwin85@gmail.com>
2020-08-03 11:37:31 -0600

    Show method now shows flight, adults, and minors

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,7 @@ class FlightsController < ApplicationController
 
   def show
     @flight = Flight.find(params[:id])
+    @adults = Passenger.where("age >= ?", 18)
+    @minors = Passenger.where("age < ?", 18)
   end
 end
