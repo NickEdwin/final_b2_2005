838f7cd47d56c2ac64c6f954fd51fe1cfe0ffc4c
Nick Edwin <nickedwin85@gmail.com>
2020-08-03 11:49:15 -0600

    Fixed possible bug when trying to add passenger to flight that doesnt exist

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -3,7 +3,12 @@ class FlightPassengersController < ApplicationController
  def create
    @flight = Flight.find_by(number: params[:number])
    @passenger = Passenger.find(params[:id])
-   @flight_passenger =  FlightPassenger.create(flight_id: @flight.id, passenger_id: @passenger.id)
-   redirect_to "/passengers/#{@passenger.id}"
+   if @flight.nil?
+     flash[:errors] = "Flight must exist! "
+     redirect_to "/passengers/#{@passenger.id}"
+   else
+     @flight_passenger =  FlightPassenger.create(flight_id: @flight.id, passenger_id: @passenger.id)
+     redirect_to "/passengers/#{@passenger.id}"
+   end
  end
 end
