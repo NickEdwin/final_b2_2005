9a080920f52e198cdafee3a621387e39a5fb258d
Nick Edwin <nickedwin85@gmail.com>
2020-08-03 09:44:12 -0600

    Updates spacing in test

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -6,7 +6,6 @@ describe Airline, type: :model do
   end
 
   describe "relationships" do
-    it {should have_many :flights} 
-    # it {should have_many(:passengers).through(:flights)}
+    it {should have_many :flights}
   end
 end
